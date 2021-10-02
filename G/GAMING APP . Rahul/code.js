b0c8ae2f433d41a4a6ac2889b37aec33850411b6
Rahul singh12 <72645633+rahul12singh@users.noreply.github.com>
2021-10-02 16:22:21 +0530

    Add files via upload

---
+++ G/GAMING APP . Rahul/code.js
@@ -0,0 +1,40 @@
+onEvent("button12", "click", function( ) {
+  open("https://www.youtube.com/channel/UCtJ566i2MIhSMSfOa0dJ0Lw");
+});
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button2", "click", function( ) {
+  open("https://html5.gamemonetize.com/luye5rleios737wp2o9wjq7h577gl937/");
+  write("thnka");
+});
+onEvent("button3", "click", function( ) {
+  open("https://www.h5gamesscreen.com/8ball-pool");
+});
+onEvent("button5", "click", function( ) {
+  open("https://g.shtoss.com/trio-online/index.html");
+});
+onEvent("button4", "click", function( ) {
+  open("https://g.shtoss.com/bottle-flip-challenge/index.html");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button7", "click", function( ) {
+  open("https://g.shtoss.com/dots-pong/index.html");
+});
+onEvent("button10", "click", function( ) {
+  open("https://g.shtoss.com/scared-silly/index.html");
+});
+onEvent("button8", "click", function( ) {
+  open("https://g.shtoss.com/circuit-drifter/index.html");
+});
+onEvent("button9", "click", function( ) {
+  open("https://g.shtoss.com/project-archer/index.html");
+});
+onEvent("button11", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button12", "click", function( ) {
+  setScreen("screen1");
+});
